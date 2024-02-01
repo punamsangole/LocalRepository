cac33e6f35c6975e09b1baed368137785efb2a29
punamsangole <131657320+punamsangole@users.noreply.github.com>
2024-02-01 12:56:20 +0530

    commit change

--- Shraddha/src/Addition.java
+++ Shraddha/src/Addition.java
@@ -2,6 +2,10 @@
 public class Addition {
 	public static void main(String[] args) {
 		System.out.println("java project");
+		System.out.println("java project");
+		System.out.println("java project");
+		System.out.println("java project");
+		
 	}
 
 }
